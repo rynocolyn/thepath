c9d58fd3710a1eb95ff279779382e9a1d25351cf
carelvwyk <carel@bitx.co>
2015-09-28 17:41:20 +0200

    Busy creating first endpoint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var mysql     =    require('mysql');
-var app = express();
+var mysql   = require('mysql');
+var app     = express();
 
 // +DB
 var pool      =    mysql.createPool({
@@ -12,43 +12,84 @@ var pool      =    mysql.createPool({
     debug    :  false
 });
 
-function dbquery(req,res,query) {
+function dbquery(query, callback) {
   pool.getConnection(function(err,connection){
     if (err) {
       connection.release();
-      res.json({"code" : 100, "status" : "Error connecting to database"});
-      return null;
+      callback("error connecting to database", null);
+      return;
     }
 
-    console.log('connected as id ' + connection.threadId);
-
-    connection.query(query,function(err,rows){
+    connection.query(query, function(err,rows){
       connection.release();
-      if(!err) {
-        res.json(rows);
-      }
+      callback(err, rows);
+      return;
     });
 
     connection.on('error', function(err) {
-      res.json({"code" : 100, "status" : "Error connecting to database"});
-      return null;
+      callback(err, null);
+      return;
     });
   });
-}
-
+};
 // -DB
 
-// +Server
-app.get('/', function (req, res) {
-	dbquery(req, res, "select * from questions;")
+// +Other Functions
+function showFirstQuestion(response) {
+  dbquery("select id, question_text from questions where id=1;",
+    function(err, data) {
+      response.json({id: data[0].id, text: data[0].question_text});
+    });
+}
+// -Other Functions
 
+// +Routing
+
+// Get Next Question Endpoint:
+// Two parameters:
+// 1. current question's id
+// 2. answer (bool)
+app.get('/next_question', function (request, response) {
+  // Get parameters:
+  var currentQuestionId = request.query.current_id;
+  var answer = request.query.answer;
+
+  // Validate parameters:
+  // -- skipped --
+
+  dbquery("select * from questions where id="+currentQuestionId+";",
+    function (err, data) {
+      if (!data || len(data)==0) {
+        showFirstQuestion(response);
+        return;
+      }
+
+      /*if (answer == true) {
+        dbquery("select yes_question_index from questions where "+
+          " id=" + currentQuestionId + ";", function(err, data) {
+        });
+      } else {
+        // Get no question id
+      }*/
+    });
+
+  /*var rows = dbquery("select * from questions;", function(err, rows) {
+    if (err != null) {
+      response.json({"error": err});
+    } else {
+      response.json(rows);
+    }
+  });
+  console.log(rows);*/
+  //response.json(rows);
 });
+// -Routing
 
+// +Server
 var server = app.listen(6060, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
 // -Server
